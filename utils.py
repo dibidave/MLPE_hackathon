a6a5bdeb5a4494c5d5faaab440134f839e02047c
David Brown <dibidave@gmail.com>
2018-10-13 12:49:29 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 def convert_amino_acid_ids_to_letters(number_sequences):
+        
+    amino_acids = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
     
     amino_acid_sequences = []
     
     for number_sequence in number_sequences:
-        
-        amino_acids = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
 
         amino_acid_sequence = [amino_acids[i - 1] for i in number_sequence]
 
